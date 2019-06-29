52d00b0486b1a09cca2fdcb7d347ca2d65df4ce7
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-06-30 00:51:38 +0530

    All Unit tests complete

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -1,14 +1,90 @@
 using System;
 using Xunit;
+using BowlingBall;
 
 namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        public Game game = new Game();
         [Fact]
         public void DummyTest()
         {
             // This is a dummy test that will always pass.
         }
+
+        // Test for normal Roll
+        [Fact]
+        
+        public void NormalRollTest()
+        {
+            int[] rolls = new int[] { 2,2 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(4, score);   
+        }
+
+        // Test for Strike
+        [Fact]
+        public void StrikeOnlyTest()
+        {
+            int[] rolls = new int[] { 8, 1, 10, 6, 2 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(35, score);
+        }
+        // Test for Spare
+        [Fact]
+        public void SpareOnlyTest()
+        {
+            int[] rolls = new int[] { 8, 2, 8, 1, 6, 2 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(35, score);
+        }
+        // Test for Strike then Spare
+        [Fact]
+        public void StrikeThenSpareTest(){
+            int[] rolls = new int[] { 10, 2, 8, 1, 6 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(38, score);
+        }
+        // Test for Spare then Strike
+        [Fact]
+        public void SpareThenStrike(){
+            int[] rolls = new int[] { 2, 8, 10, 1, 6 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(44, score);
+        }
+        // Test for Strike on Last Roll
+        [Fact]
+        public void StrikeOnLastRoll()
+        {
+            int[] rolls = new int[] { 2, 6, 10, 1, 8, 10 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(46, score);
+        }
+        // Test for Spare on Last Roll
+        [Fact]
+        public void SpareOnLastRoll()
+        {
+            int[] rolls = new int[] { 2, 8, 10, 2, 8 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(50, score);
+        }
+        // Test for all strikes on last Roll
+        [Fact]
+        public void AllStrikes()
+        {
+            int[] rolls = new int[] { 10, 10, 10, 10, 10 };
+            game.InitializeRoll(rolls);
+            int score = game.GetScore();
+            Assert.Equal(120, score);
+        }
+
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -30,27 +30,64 @@ namespace BowlingBall
                 if(rolls[rollCounter] == 10)
                 {
                     // adding 10 with score of next two rolls
-                    score += 10 + rolls[rollCounter + 1] + rolls[rollCounter + 2];
-                    frames[frameCounter] = 10 + rolls[rollCounter + 1] + rolls[rollCounter + 2];
+                    if(rolls.Length > rollCounter + 2)
+                    {
+                        score += 10 + rolls[rollCounter + 1] + rolls[rollCounter + 2];
+                        frames[frameCounter] = 10 + rolls[rollCounter + 1] + rolls[rollCounter + 2];
+                    }
+
+                    else if (rollCounter + 2 == rolls.Length)
+                    {
+                        score += 10 + rolls[rollCounter + 1];
+                        frames[frameCounter] = 10 + rolls[rollCounter + 1];
+                    }
+                    else
+                    {
+                        score += 10;
+                        frames[frameCounter] = 10;
+                    }
+                    
 
                     // incrementing rollCounter by 1 as next roll will be for next frame
                     rollCounter += 1;
                 }
                 // Condition for a spare
-                else if((rolls[rollCounter] + rolls[rollCounter + 1] ) == 10){
-                    
-                    // adding 10 with score of next roll i.e. first roll of next frame
-                    score += 10 + rolls[rollCounter + 2];
-                    frames[frameCounter] = 10 + rolls[rollCounter + 2];
+                else if(rolls.Length > rollCounter + 1 && (rolls[rollCounter] + rolls[rollCounter + 1] ) == 10){
 
+                    // If this is not the last roll
+                    // adding 10 with score of next roll i.e. first roll of next frame
+                    if (rolls.Length > rollCounter + 2)
+                    {
+                        score += 10 + rolls[rollCounter + 2];
+                        frames[frameCounter] = 10 + rolls[rollCounter + 2];
+                    }
+                    // If this is the last roll
+                    // Add 10 for the current and last roll
+                    else
+                    {
+                        score += 10;
+                        frames[frameCounter] = 10;
+                    }
+                    
                     // incrementing rollCounter by 2 as 2 rolls are of same frame and next roll will be in the next frame
                     rollCounter += 2;
                 }
                 // Condition for normal roll
                 else
                 {
-                    score += rolls[rollCounter] + rolls[rollCounter + 1];
-                    frames[frameCounter] = rolls[rollCounter] + rolls[rollCounter + 1];
+                    // if this is not the last roll
+                    if(rolls.Length > rollCounter + 1)
+                    {
+                        score += rolls[rollCounter] + rolls[rollCounter + 1];
+                        frames[frameCounter] = rolls[rollCounter] + rolls[rollCounter + 1];
+                    }
+                    // if this is the last roll
+                    else
+                    {
+                        score += rolls[rollCounter];
+                        frames[frameCounter] = rolls[rollCounter];
+                    }
+                    
                     rollCounter += 2;
                 }
                 //
